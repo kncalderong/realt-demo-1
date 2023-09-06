ad294310c85407d99d77bf3b63f4e2c0c49c7c17
Kevin Nicolas <kncalderong@gmail.com>
2023-09-06 17:48:52 -0500

    feat: Header for mobile devices developed

---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import Navbar from './Navbar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons/faCartShopping'
+import { faUserCircle } from '@fortawesome/free-regular-svg-icons'
+const Header = () => {
+  return (
+    <header className='fixed z-10 flex items-center w-full gap-6 px-6 py-3 bg-gradient-to-b from-realt-blue-250 to-white'>
+      <div className='block lg:hidden'>
+        <Navbar />
+      </div>
+      <Link href='/' className='flex justify-center grow'>
+        <Image
+          alt='realT-logo'
+          src={'/realt_logo.svg'}
+          width={125}
+          height={65}
+        />
+      </Link>
+      <Link href='/'>
+        <FontAwesomeIcon
+          icon={faCartShopping}
+          className='w-[25px] h-[25px] text-realt-dark-blue'
+        />
+      </Link>
+      <Link href='/'>
+        <FontAwesomeIcon
+          icon={faUserCircle}
+          className='w-[25px] h-[25px] text-realt-dark-blue'
+        />
+      </Link>
+    </header>
+  )
+}
+
+export default Header
