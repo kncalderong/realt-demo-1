54ec633b470a843e8043ae977a0044dac40538ae
Kevin Nicolas <nicolas.calderon@realt.co>
2023-09-18 18:02:44 -0500

    feat: smooth scroll developed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import DefiSection from '@/components/HomeLanding/DefiSection'
 import IntroSection from '@/components/HomeLanding/IntroSection'
+import SmoothScroll from '@/components/SmoothScroll'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen pb-24 h-[200vh] relative'>
-      <div className='flex flex-col justify-start items-center w-[90%] mx-auto'>
+    <main className='flex min-h-screen pb-24 relative'>
+      <div className='flex flex-col justify-start items-center w-[90%] mx-auto gap-16 md:gap-20'>
+        <SmoothScroll />
         <IntroSection />
+        <DefiSection />
       </div>
     </main>
   )
